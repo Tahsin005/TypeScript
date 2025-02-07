099a22a61d27893e5b66b038f3bffe23f5b1c112
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-07 15:14:22 +0600

    1 - primitive and reference types - overview

---
+++ 1_basic_types/app.ts
@@ -0,0 +1,18 @@
+/*
+list of the primitive types in TypeScript:
+
+    boolean: true or false
+    number: Represents numeric values (integers, floating-point numbers, etc.). All numbers are essentially floating-point in JavaScript/TypeScript.
+    string: A sequence of characters.
+    null: Represents the intentional absence of a value.
+    undefined: Represents a variable that has been declared but has not been assigned a value.
+    symbol (ES6): Creates unique identifiers.
+    bigint (ES2020): For integers of arbitrary size.
+
+reference types are:
+
+    object: A general type for non-primitive values.
+    array: An ordered collection of values.
+    function: A callable object.
+    Classes: Templates for creating objects.
+*/
