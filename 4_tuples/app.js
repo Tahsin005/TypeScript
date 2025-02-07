b3830aa259ad5509f14a512f69b73d1ed87130e6
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-07 15:47:37 +0600

    4 - tuples

---
+++ 4_tuples/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let arr = ['tahsin', 23];
+let arr2;
+arr2 = [22, 'tahsin'];
---
+++ 4_tuples/app.ts
@@ -0,0 +1,6 @@
+let arr: [string, number] = ['tahsin', 23];
+
+let arr2: [number, string];
+
+
+arr2 = [22, 'tahsin'];
