26a3f1b7433694658d9e8a7f0e195317def86d24
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-07 15:36:43 +0600

    3 - arrays

---
+++ 3_arrays/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let arr = [];
+let number = [1, 2, 3];
+let mixed = [1, 'tahsin', true, { name: 'John', age: 23 }];
---
+++ 3_arrays/app.ts
@@ -0,0 +1,3 @@
+let arr = [];
+let number = [1, 2, 3];
+let mixed = [1, 'tahsin', true, { name: 'John', age: 23 }];
