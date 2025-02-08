e7782b8a13fc198bbcabea80c0437381b1b355e6
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-08 12:01:28 +0600

    13 - functions

---
+++ 13_functions/app.js
@@ -0,0 +1,32 @@
+function abcd() {
+    console.log('Hi');
+}
+function abcdef(name, age, cb) {
+    cb('hello');
+}
+abcdef('tahsin', 23, function (arg) {
+    console.log(arg);
+});
+// optional parameters
+function ghijkl(name, age, gender) {
+    if (gender === void 0) { gender = 'male or female r kichu hoile thabor dibo'; }
+    console.log(name, age, gender);
+}
+ghijkl('tahsin', 23, 'male');
+ghijkl('lagbatak', 18);
+// rest parameters
+function mnopqrs(name) {
+    var arr = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        arr[_i - 1] = arguments[_i];
+    }
+    console.log(name, arr);
+}
+mnopqrs('tahsin', 1, 2, 3, 4, 5);
+function combine(input1, input2) {
+    return input1 + input2;
+}
+// Usage
+var result1 = combine("Hello, ", "World!"); // "Hello, World!"
+var result2 = combine(10, 20); // 30
+// const result3 = combine("Hello, ", 10); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
---
+++ 13_functions/app.ts
@@ -0,0 +1,39 @@
+function abcd(): void {
+    console.log('Hi');
+}
+
+function abcdef(name: string, age: number, cb: (arg: string) => void) {
+    cb('hello')
+}
+
+abcdef('tahsin', 23, (arg: string) => {
+    console.log(arg);
+})
+
+// optional parameters
+function ghijkl(name: string, age?: number, gender: string = 'male or female r kichu hoile thabor dibo'): void {
+    console.log(name, age, gender);
+}
+
+ghijkl('tahsin', 23, 'male')
+ghijkl('lagbatak', 18);
+
+// rest parameters
+function mnopqrs(name: string, ...arr: number[]): void {
+    console.log(name, arr);
+}
+
+mnopqrs('tahsin',1, 2, 3, 4, 5);
+
+// function overloading
+function combine(input1: string, input2: string): string; // Concatenate strings
+function combine(input1: number, input2: number): number; // Add numbers
+
+function combine(input1: any, input2: any): any {
+    return input1 + input2;
+}
+
+// Usage
+const result1 = combine("Hello, ", "World!"); // "Hello, World!"
+const result2 = combine(10, 20); // 30
+// const result3 = combine("Hello, ", 10); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
